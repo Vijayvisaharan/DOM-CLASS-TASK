6b5569fd05756af91abeccdeefd138205d61d058
VIJAY S <157093385+Vijayvisaharan@users.noreply.github.com>
2024-02-20 12:14:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function foo(){
+    var firstname=document.getElementById("firstname").value
+    console.log(firstname)
+    var middlename=document.getElementById("middlename").value
+    console.log(middlename)
+    var lastname=document.getElementById("lastname").value
+    console.log(lastname)
+        var email=document.getElementById("email").value
+        console.log(email)
+
+}
+
+
